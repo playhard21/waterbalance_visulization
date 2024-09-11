6d4367f40b37964c4b93b39a65a383bf158aa86e
Aravind <aravind.karri@dedrone.com>
2024-09-12 00:21:18 +0200

    working code with all results

--- main.py
+++ main.py
@@ -11,15 +11,30 @@ gdf = gpd.read_file('data/Sub_basins.geojson')
 
 def load_et_values():
     # Load ET ranges
-    with open('data/percolation_data.json', 'r') as f:
+    # step - 01
+    with open('data/et_vis_data.json', 'r') as f:
         et_ranges = json.load(f)
     return et_ranges, gdf
+
+
+# take colors from here
+"""
+'brown', [(0.8, 0.52, 0.25), (0.5, 0.25, 0.1)]  # Brown shades
+'skyblue', [(0.6, 0.8, 1.0), (0.2, 0.6, 1.0)]  # Sky blue shades
+'red', [(1.0, 0.8, 0.8), (1.0, 0.2, 0.2)]  # Red shades
+
+
+Runoff
+Percolation
+Evapotranspiration
+"""
 
 
 def get_colormap():
     # Define a custom brown colormap
     color_of_graph = mcolors.LinearSegmentedColormap.from_list(
-        'blue', [(0.8, 0.52, 0.25), (0.5, 0.25, 0.1)]  # Brown shades
+        # step - 02 paste the color here
+        'red', [(1.0, 0.8, 0.8), (1.0, 0.2, 0.2)]  # Red shades
     )
     return color_of_graph
 
@@ -35,7 +50,8 @@ def plot_et(frame, ax3, gdf, et_ranges, norm):
     gdf['ET'] = et_values
     color_of_graph = get_colormap()
     gdf.plot(column='ET', cmap=color_of_graph, linewidth=0.8, ax=ax3, edgecolor='black', legend=False, norm=norm)
-    ax3.set_title(f'Percolation')
+    # step - 03 change the name here
+    ax3.set_title(f'Evapotranspiration')
     ax3.set_axis_off()
 
     return month
@@ -96,7 +112,8 @@ ani = animation.FuncAnimation(fig, update,
                               repeat=False)
 
 # Save the animation using Pillow
-ani.save(f'percolation.gif', writer='pillow')
+# Step - 04 change the name of the gif here
+ani.save(f'Evapotranspiration.gif', writer='pillow')
 
 # Show the plot
 plt.show()
