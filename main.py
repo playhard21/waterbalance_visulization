23e7fddc1c061cfa9049de9274f46c98be80a122
Aravind <aravind.karri@dedrone.com>
2024-09-12 00:02:43 +0200

    put all the logic in main

--- main.py
+++ main.py
@@ -1,11 +1,49 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import numpy as np
 from matplotlib.ticker import FuncFormatter
-from src.evapotraspiration import load_et_values, plot_et, get_colormap
+from src.evapotraspiration import load_et_values, plot_et, get_colormap, get_global_min_max
+import geopandas as gpd
+import json
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import numpy as np
 
-# Set the plot type (et or percolation_and_surface)
-plot_type = 'et'
+
+gdf = gpd.read_file('data/Sub_basins.geojson')
+
+
+def load_et_values():
+    # Load ET ranges
+    with open('data/percolation_data.json', 'r') as f:
+        et_ranges = json.load(f)
+
+    return et_ranges, gdf
+
+
+def get_colormap():
+    # Define a custom brown colormap
+    color_of_graph = mcolors.LinearSegmentedColormap.from_list(
+        'blue', [(0.8, 0.52, 0.25), (0.5, 0.25, 0.1)]  # Brown shades
+    )
+    return color_of_graph
+
+
+def plot_et(frame, ax3, gdf, et_ranges):
+    month = list(et_ranges.keys())[frame]
+
+    # Get ET values
+    et_values = [et_ranges[month][str(i + 1)] for i in range(len(gdf))]
+
+    # Plot ET values
+    ax3.clear()
+    gdf['ET'] = et_values
+    # brown_cmap = create_brown_colormap()
+    color_of_graph = get_colormap()
+    gdf.plot(column='ET', cmap=color_of_graph, linewidth=0.8, ax=ax3, edgecolor='black', legend=False)
+    ax3.set_title(f'Precolation')
+    ax3.set_axis_off()
+
+    return month
 
 
 # Calculate global min and max for the color scale
@@ -18,45 +56,43 @@ def get_global_min_max(et_ranges_values):
 
     return global_min, global_max
 
+# Load data for evapotranspiration
+et_ranges, gdf = load_et_values()
 
-if plot_type == 'et':
-    # Load data for evapotranspiration
-    et_ranges, gdf = load_et_values()
+# Calculate global min and max
+global_min, global_max = get_global_min_max(et_ranges)
 
-    # Calculate global min and max
-    global_min, global_max = get_global_min_max(et_ranges)
-
-    # Create a figure with one subplot for evapotranspiration
-    fig, ax3 = plt.subplots(1, 1, figsize=(12, 8))  # Reduced figure height
+# Create a figure with one subplot for evapotranspiration
+fig, ax3 = plt.subplots(1, 1, figsize=(12, 8))  # Reduced figure height
 
 
-    def update(frame):
-        # Plot ET values
-        month = plot_et(frame, ax3, gdf, et_ranges)
-        fig.suptitle(f'{month}', fontsize=20)
+def update(frame):
+    # Plot ET values
+    month = plot_et(frame, ax3, gdf, et_ranges)
+    fig.suptitle(f'{month}', fontsize=20)
 
 
-    # Set the color limits for consistency across frames
-    norm = plt.Normalize(vmin=global_min, vmax=global_max)
+# Set the color limits for consistency across frames
+norm = plt.Normalize(vmin=global_min, vmax=global_max)
 
-    # Create the colorbar with a custom size
-    # The arguments are [left, bottom, width, height]
-    cbar_ax = fig.add_axes([0.88, 0.25, 0.01, 0.4])  # Adjust height and place the colorbar as close as possible
+# Create the colorbar with a custom size
+# The arguments are [left, bottom, width, height]
+cbar_ax = fig.add_axes([0.88, 0.25, 0.01, 0.4])  # Adjust height and place the colorbar as close as possible
 
-    # Create the colorbar
-    color_of_graph = get_colormap()
-    sm = plt.cm.ScalarMappable(cmap=color_of_graph, norm=norm)
-    sm.set_array([])  # Required for colorbar
-    cbar = fig.colorbar(sm, cax=cbar_ax)
-
-
-    # Add the unit "MM" to each tick label
-    def add_mm_to_ticks(x, _):
-        return f'{int(x)}mm'  # Converts tick values to strings with 'MM' appended
+# Create the colorbar
+color_of_graph = get_colormap()
+sm = plt.cm.ScalarMappable(cmap=color_of_graph, norm=norm)
+sm.set_array([])  # Required for colorbar
+cbar = fig.colorbar(sm, cax=cbar_ax)
 
 
-    # Set tick labels with "MM" after each value
-    cbar.ax.yaxis.set_major_formatter(FuncFormatter(add_mm_to_ticks))
+# Add the unit "MM" to each tick label
+def add_mm_to_ticks(x, _):
+    return f'{int(x)}mm'  # Converts tick values to strings with 'MM' appended
+
+
+# Set tick labels with "MM" after each value
+cbar.ax.yaxis.set_major_formatter(FuncFormatter(add_mm_to_ticks))
 
 # Minimize padding between the plot and colorbar
 plt.subplots_adjust(wspace=0.01)  # Very small padding between the plot and the colorbar
