e93ee27a9830aad4dfaa889d1974b7376f7a9852
Aravind <aravind.karri@dedrone.com>
2024-09-12 00:24:27 +0200

    added steps to the cleaned logic

--- main.py
+++ main.py
@@ -15,6 +15,19 @@ def load_data():
     return gdf, et_ranges
 
 
+# take colors from here
+"""
+'brown', [(0.8, 0.52, 0.25), (0.5, 0.25, 0.1)]  # Brown shades
+'skyblue', [(0.6, 0.8, 1.0), (0.2, 0.6, 1.0)]  # Sky blue shades
+'red', [(1.0, 0.8, 0.8), (1.0, 0.2, 0.2)]  # Red shades
+
+
+Runoff
+Percolation
+Evapotranspiration
+"""
+
+
 # Define custom colormap
 def get_colormap(name, colors):
     return mcolors.LinearSegmentedColormap.from_list(name, colors)
@@ -27,6 +40,7 @@ def plot_et(frame, ax, gdf, et_ranges, norm, colormap):
     gdf['ET'] = et_values
     ax.clear()
     gdf.plot(column='ET', cmap=colormap, linewidth=0.8, ax=ax, edgecolor='black', legend=False, norm=norm)
+    # step - 02 change the name here
     ax.set_title(f'Runoff')
     ax.set_axis_off()
     return month
@@ -44,6 +58,7 @@ global_min, global_max = get_global_min_max(et_ranges)
 
 # Create figure and color map
 fig, ax = plt.subplots(1, 1, figsize=(12, 8))
+# step - 03 change the name here
 colormap = get_colormap('skyblue', [(0.6, 0.8, 1.0), (0.2, 0.6, 1.0)])
 norm = plt.Normalize(vmin=global_min, vmax=global_max)
 
@@ -70,5 +85,6 @@ def update(frame):
 ani = animation.FuncAnimation(fig, update, frames=len(et_ranges), interval=1000, repeat=False)
 
 # Save and show animation
+# step - 04 change the name here
 ani.save('percolation.gif', writer='pillow')
 plt.show()
