59737cc1a598770e41f31d99366d84988f0818c8
Aravind <aravind.karri@dedrone.com>
2024-09-11 22:31:38 +0200

    working scale sizing

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from src.evapotraspiration import load_et_values, plot_et
 # Set the plot type (et or percolation_and_surface)
 plot_type = 'et'
 
+
 # Calculate global min and max for the color scale
 def get_global_min_max(percolation_ranges, surface_runoff_ranges, et_ranges):
     # Combine all values across all months for each dataset
@@ -21,6 +22,7 @@ def get_global_min_max(percolation_ranges, surface_runoff_ranges, et_ranges):
 
     return global_min, global_max
 
+
 if plot_type == 'percolation_and_surface':
     # Load data for percolation and surface runoff
     gdf, percolation_ranges, surface_runoff_ranges = load_percolation_surface()
@@ -33,6 +35,7 @@ if plot_type == 'percolation_and_surface':
 
     # Create a figure with two subplots for percolation and surface runoff
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+
 
     def update(frame):
         # Plot percolation and surface runoff
@@ -50,10 +53,12 @@ elif plot_type == 'et':
     # Create a figure with one subplot for evapotranspiration
     fig, ax3 = plt.subplots(1, 1, figsize=(12, 8))  # Reduced figure height
 
+
     def update(frame):
         # Plot ET values
         month = plot_et(frame, ax3, gdf, et_ranges)
         fig.suptitle(f'{month}', fontsize=20)
+
 
     # Set the color limits for consistency across frames
     norm = plt.Normalize(vmin=global_min, vmax=global_max)
@@ -67,9 +72,11 @@ elif plot_type == 'et':
     sm.set_array([])  # Required for colorbar
     cbar = fig.colorbar(sm, cax=cbar_ax)
 
+
     # Add the unit "MM" to each tick label
     def add_mm_to_ticks(x, _):
         return f'{int(x)} MM'  # Converts tick values to strings with 'MM' appended
+
 
     # Set tick labels with "MM" after each value
     cbar.ax.yaxis.set_major_formatter(FuncFormatter(add_mm_to_ticks))
